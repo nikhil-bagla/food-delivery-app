8499659e9b95720bdb3042c2e4ad00def85e056b
nikhil-bagla <nikhilbagla707@gmail.com>
2021-05-25 05:05:03 +0530

    inital commit

---
+++ models/planModel.js
@@ -0,0 +1,121 @@
+const mongoose = require("mongoose");
+// Database link
+// const config = require("../configs/config");
+// database connection
+const DB="mongodb+srv://admin:123abc@cluster0-mfg4k.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+  .then(conn => {
+    console.log("Plan DB connected");
+    // console.log(conn);
+  });
+
+// structure => Plan
+const planSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter Name of the plan"],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 5
+  },
+  averageRating: {
+    type: Number,
+    default: 5
+  },
+  description: {
+    type: String,
+    default: "Good Plan"
+  },
+  prefrence: {
+    type: String,
+    enum: ["Vegan", "Vegetarian", "Non-Veg", "Eggiterian"]
+  },
+  price: {
+    type: Number,
+    min: 40
+  },
+  duration: {
+    type: Number,
+    default: 30
+  },
+  cover:{
+    type:"String",
+    required:true
+  },
+  pictures:{
+type:[String],
+required:true
+  }
+});
+// model
+const planModel = mongoose.model("planModel", planSchema);
+module.exports = planModel;
+
+
+
+
+
+// const mongoose = require("mongoose");
+// // Database link
+// // const config = require("../configs/config");
+// // database connection
+// mongoose
+//   .connect(process.env.DB, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true
+//   })
+//   .then(conn => {
+//     console.log("Plan DB connected");
+//     // console.log(conn);
+//   });
+
+// // structure => Plan
+// const planSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: [true, "Please enter Name of the plan"],
+//     unique: true
+//   },
+//   rating: {
+//     type: Number,
+//     default: 5
+//   },
+//   averageRating: {
+//     type: Number,
+//     default: 5
+//   },
+//   description: {
+//     type: String,
+//     default: "Good Plan"
+//   },
+//   prefrence: {
+//     type: String,
+//     enum: ["Vegan", "Vegetarian", "Non-Veg", "Eggiterian"]
+//   },
+//   price: {
+//     type: Number,
+//     min: 40
+//   },
+//   duration: {
+//     type: Number,
+//     default: 30
+//   },
+//   cover:{
+//     type:"String",
+//     required:true
+//   },
+//   pictures:{
+// type:[String],
+// required:true
+//   }
+// });
+// // model
+// const planModel = mongoose.model("planModel", planSchema);
+// module.exports = planModel;
